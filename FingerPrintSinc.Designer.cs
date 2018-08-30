ddc07a8c53ebce0724e0461fdad50d63c9f78b65
Carlos Dario Flores <cdfn3@hotmail.com>
2018-08-29 21:03:02 -0600

    Add files via upload

---
+++ FingerPrintSinc.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace RLFingerPrintPro
+{
+    partial class FingerPrintSinc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnsinc = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnsinc
+            // 
+            this.btnsinc.Location = new System.Drawing.Point(90, 124);
+            this.btnsinc.Name = "btnsinc";
+            this.btnsinc.Size = new System.Drawing.Size(75, 23);
+            this.btnsinc.TabIndex = 0;
+            this.btnsinc.Text = "Sincronizar";
+            this.btnsinc.UseVisualStyleBackColor = true;
+            this.btnsinc.Click += new System.EventHandler(this.btnsinc_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 224);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "cont";
+            // 
+            // FingerPrintSinc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnsinc);
+            this.Name = "FingerPrintSinc";
+            this.Text = "FingerPrintSinc";
+            this.Load += new System.EventHandler(this.FingerPrintSinc_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnsinc;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ FingerPrintSinc.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RLFingerPrintPro
+{
+    public partial class FingerPrintSinc : Form
+    {
+        xService.xRRHHEmployee xFindEmployee;
+        List<xService.xRRHHFingerPrint> xListAllRegisteredSign = new List<xService.xRRHHFingerPrint>();
+        xService.Service1Client xServiceClient;
+        Timer tm = new Timer();
+        public FingerPrintSinc()
+        {
+            InitializeComponent();
+        }
+
+        public void MC(/*MemoryStream FotoGuardada*/)
+        {           
+
+            tm.Stop();   //  1800000;   
+            //120000 = 2 minutos  
+            tm.Interval = 360000;//tenia 3840000 //pa prueba 120000
+            tm.Tick += timerTick;
+            //pbHuella.Visible = true;
+            tm.Enabled = true;
+            tm.Start();
+
+        }
+
+        private void timerTick(object sender, EventArgs e)
+        {
+            // pbHuella.Visible = false;
+            
+            ((Timer)sender).Stop();
+            btnsinc.PerformClick();
+            //this.Close();
+            // label1.Text = "Esperando Huella";
+        }
+
+        private void FingerPrintSinc_Load(object sender, EventArgs e)
+        {
+            
+
+            string fecha = "Pendientes ";
+            try
+            {
+                xServiceClient = new xService.Service1Client();
+                fecha += xServiceClient.xGetHorasPend(xServiceClient.xGetServerTime().ToString("yyyyMMdd"));
+                label1.Text = fecha;
+            }
+            catch (Exception ex)
+            {
+               // MessageBox.Show(ex.ToString());
+                label1.Text = ex.ToString();
+            }
+            finally
+            {
+                xServiceClient.Close();
+                xServiceClient.Abort();
+            }
+            
+            MC();
+        }
+        //por aqui vas tratando de sincornizar marcaje a as400               
+        //idea   --primero select  al servidor el server aacomoda los valores pa ingresar al as400, ingresa y cambia estado en local
+        private void btnsinc_Click(object sender, EventArgs e)
+        {
+            string fecha = "Pendientes ";
+            try
+            {
+                xServiceClient = new xService.Service1Client();
+                fecha += xServiceClient.xSincHoras(xServiceClient.xGetServerTime().ToString("yyyyMMdd"));//
+                /*crear la opcion de sincronizar y al terminar mostrar msj*/
+                label1.Text = fecha + " Sincronizacion terminada.";
+                this.Close();
+                
+                //MessageBox.Show("Sincronizacion terminada");
+            }
+            catch (Exception ex)
+            {
+               //MessageBox.Show(ex.ToString());
+                label1.Text = ex.ToString();
+            }
+            finally
+            {
+                xServiceClient.Close();
+                xServiceClient.Abort();
+            }
+
+            MC();
+             
+        }
+    }
+}
